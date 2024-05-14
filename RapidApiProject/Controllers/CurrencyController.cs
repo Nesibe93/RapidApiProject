571c010560ca8394a5744a6511151e5a60652888
Nesibe93 <125722258+Nesibe93@users.noreply.github.com>
2024-05-14 11:51:11 +0300

    Add project files.

---
+++ RapidApiProject/Controllers/CurrencyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+
+namespace RapidApiProject.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com15.p.rapidapi.com/api/v1/meta/getExchangeRates?base_currency=TRY"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "37a7d71a37msh26fee5c4314fa6dp1d1043jsn0c26a3096c90" },
+        { "X-RapidAPI-Host", "booking-com15.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.data.exchange_rates.ToList()); // Benim istediğim değerler exchange_rates in içinde olduğu için böyle yazdık
+            }
+        }
+    }
+}
---
+++ RapidApiProject/Controllers/ImdbTop100MovieController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+using System.Text.Json.Serialization;
+
+namespace RapidApiProject.Controllers
+{
+    public class ImdbTop100MovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "37a7d71a37msh26fee5c4314fa6dp1d1043jsn0c26a3096c90" }, // Api Keyimiz
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" }, // İstek yapılacak sunucu
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<MovieViewModel>>(body);
+                return View(values);
+            }
+        }
+    }
+}
